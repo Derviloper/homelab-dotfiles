017a096c26094b90e36e0b39f4f4caffde764a7a
Derviloper <derviloper@gmx.de>
2026-07-10 13:43:26 +0200

    Added availability under homelab.local

--- hosts/homelab/default.nix
+++ hosts/homelab/default.nix
@@ -42,6 +42,17 @@
 
   services.fail2ban.enable = true;
 
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
+  };
+
   networking.firewall = {
     allowedTCPPorts = [ 22 ];
   };
